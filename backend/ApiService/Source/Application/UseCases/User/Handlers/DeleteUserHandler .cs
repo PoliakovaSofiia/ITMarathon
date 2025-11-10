fd112ceaeaf273c7e73911ac0fe084124e137fa3
PoliakovaSofiia <victorica123121@gmail.com>
2025-11-10 19:39:52 +0200

    delete user

---
+++ backend/ApiService/Source/Application/UseCases/User/Handlers/DeleteUserHandler .cs
@@ -0,0 +1,49 @@
+﻿using CSharpFunctionalExtensions;
+using Epam.ItMarathon.ApiService.Application.UseCases.User.Commands;
+using Epam.ItMarathon.ApiService.Application.UseCases.User.Queries;
+using Epam.ItMarathon.ApiService.Domain.Abstract;
+using Epam.ItMarathon.ApiService.Domain.Shared.ValidationErrors;
+using FluentValidation.Results;
+using MediatR;
+using RoomAggregate = Epam.ItMarathon.ApiService.Domain.Aggregate.Room.Room;
+
+namespace Epam.ItMarathon.ApiService.Application.UseCases.User.Handlers
+{
+    public class DeleteUserHandler(IRoomRepository roomRepository)
+        : IRequestHandler<DeleteUsersRequest, Result<RoomAggregate, ValidationResult>>
+    {
+        ///<inheritdoc/>
+        public async Task<Result<RoomAggregate, ValidationResult>> Handle(DeleteUsersRequest request,
+            CancellationToken cancellationToken)
+        {
+            // 1. Get room by user code
+            var roomResult = await roomRepository.GetByUserCodeAsync(request.UserCode, cancellationToken);
+            if (roomResult.IsFailure)
+            {
+                return roomResult;
+            }
+
+            // 2. Delete user by id from room - room.DeleteUser(userId)
+            var room = roomResult.Value;
+            var deleteResult = room.DeleteUser(request.UserId);
+            if (deleteResult.IsFailure)
+            {
+                return deleteResult;
+            }
+
+            // 3. Update room in repository
+            var updateResult = await roomRepository.UpdateAsync(room, cancellationToken);
+            if (updateResult.IsFailure)
+            {
+                return Result.Failure<RoomAggregate, ValidationResult>(new BadRequestError([
+                    new ValidationFailure(string.Empty, updateResult.Error)
+                ]));
+            }
+
+            // 4. Get updated room
+            var updatedroomResult = await roomRepository.GetByUserCodeAsync(request.UserCode, cancellationToken);
+            return updatedroomResult;
+
+        }
+    }
+}
